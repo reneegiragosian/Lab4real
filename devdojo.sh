90ce2216813ae3bcf8da6964effb4d089e926bbe
reneegiragosian <149642357+reneegiragosian@users.noreply.github.com>
2023-12-14 06:19:30 +0000

    git init

--- devdojo.sh
+++ devdojo.sh
@@ -1,16 +1,44 @@
+# !/bin/bash
+users="devdojo bobby tony"
+for user in ${users}
+do
+ echo "${user}"
+done
 #!/bin/bash
-read -p "Enter the name of your car brand: " car
-case $car in
- Tesla)
- echo -n "${car}'s car factory is in the USA."
- ;;
- BMW | Mercedes | Audi | Porsche)
- echo -n "${car}'s car factory is in Germany."
- ;;
- Toyota | Mazda | Mitsubishi | Subaru)
- echo -n "${car}'s car factory is in Japan."
- ;;
- *)
- echo -n "${car} is an unknown car brand"
- ;;
-esac
+for num in {1..10}
+do
+ echo ${num}
+done
+#!/bin/bash
+counter=1
+while [[ $counter -le 10 ]]
+do
+ echo $counter
+ ((counter++))
+done
+#!/bin/bash
+read -p "What is your name? " name
+while [[ -z ${name} ]]
+do
+ echo "Your name can not be blank. Please enter a valid
+name!"
+ read -p "Enter your name again? " name
+done
+echo "Hi there ${name}"
+#!/bin/bash
+count=1
+until [[ $count -gt 10 ]]
+do
+ echo $count
+ ((count++))
+done
+# !/bin/bash
+for i in 1 2 3 4 5
+do
+ if [[ $i –eq 2 ]]
+ then
+ echo "skipping number 2"
+ continue
+ fi
+ echo "i is equal to $i"
+done
