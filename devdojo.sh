6b22bf112251520322eafa91b2b8adf7af6f27bc
reneegiragosian <149642357+reneegiragosian@users.noreply.github.com>
2023-12-14 06:14:20 +0000

    git init

--- devdojo.sh
+++ devdojo.sh
@@ -1,9 +1,44 @@
 
-# !/bin/bash
-letters=( "A""B""C""D""E" )
-echo ${letters[@]}
-
 #!/bin/bash
-letters=( "A""B""C""D""E" )
-b=${letters:3}
-echo "${b}"
+# Bash if statement example
+read -p "What is your name? " name
+if [[ -z ${name} ]]
+then
+ echo "Please enter your name!"
+else
+ echo "Hi there ${name}"
+fi
+#!/bin/bash
+admin="devdojo"
+read -p "Enter your username? " username
+# Check if the username provided is the admin
+if [[ "${username}" == "${admin}" ]] ; then
+ echo "You are the admin user!"
+else
+ echo "You are NOT the admin user!"
+fi
+#!/bin/bash
+if (( $EUID == 0 )); then
+ echo "Please do not run as root"
+ exit
+fi
+#!/bin/bash
+admin="devdojo"
+read -p "Enter your username? " username
+# Check if the username provided is the admin
+if [[ "${username}" != "${admin}" ]] || [[ $EUID != 0 ]] ;
+then
+ echo "You are not the admin or root user, but please be
+safe!"
+else
+ echo "You are the admin user! This could be very
+destructive!"
+fi#!/bin/bash
+read -p "Enter a number: " num
+if [[ $num -gt 0 ]] ; then
+ echo "The number is positive"
+elif [[ $num -lt 0 ]] ; then
+ echo "The number is negative"
+else
+ echo "The number is 0"
+f
