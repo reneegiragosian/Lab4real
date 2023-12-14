e76f268a11021c43d6430f211fee10a09830859d
reneegiragosian <149642357+reneegiragosian@users.noreply.github.com>
2023-12-14 05:47:04 +0000

    git init

--- devdojo.sh
+++ devdojo.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 name="DevDojo"
 echo "Hi there $name"
+#!/bin/bash
+name="DevDojo"
+greeting="Hello"
+echo "$greeting $name"
+!/bin/bash
+echo "Hello there" $1
+# $1 : first parameter
+echo "Hello there" $2
+# $2 : second parameter
+echo "Hello there" $@
+# $@ : all
+
+# !/bin/bash
+echo "What is your name?"
+read name
+echo "Hi there $name"
+echo "Welcome to DevDojo!"
 
 
